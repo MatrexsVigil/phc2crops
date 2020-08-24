d51abe0e2f2fae6ff579c36f7c8152511bcd00ad
MatrexsVigil <worldofro@gmail.com>
2020-08-23 22:30:07 -0400

    1.16.2-1.0.0

--- src/main/java/pam/pamhc2crops/events/TallGrassLootHandler.java
+++ src/main/java/pam/pamhc2crops/events/TallGrassLootHandler.java
@@ -1,8 +1,8 @@
 package pam.pamhc2crops.events;
 
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.TableLootEntry;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootPool;
-import net.minecraft.world.storage.loot.TableLootEntry;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import pam.pamhc2crops.Pamhc2crops;
